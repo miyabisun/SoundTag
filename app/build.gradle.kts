ea7f1ff737411e71d5947ad070b66f3df6ef7d34
miyabisun <miyabi.ooh@gmail.com>
2026-09-19 10:24:06 +0900

    release: v0.1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,8 +7,8 @@ android {
         applicationId = "dev.miyabisun.soundtag"
         minSdk = 37
         targetSdk = 37
-        versionCode = 3
-        versionName = "0.1.2"
+        versionCode = 4
+        versionName = "0.1.3"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
     compileOptions {
